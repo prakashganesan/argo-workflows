0f990e65ce89e7977094e4a38a120e8070e5dbb1
Yuan Tang <terrytangyuan@gmail.com>
2022-07-05 10:25:56 -0400

    fix: fix condition
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- ui/src/app/workflows/components/workflow-creator-info/workflow-creator-info.tsx
+++ ui/src/app/workflows/components/workflow-creator-info/workflow-creator-info.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as models from '../../../../models';
-import {labels} from "../../../../models";
+import {labels} from '../../../../models';
 
 require('./workflow-creator-info.scss');
 
@@ -17,27 +17,30 @@ export class WorkflowCreatorInfo extends React.Component<WorkflowCreatorInfoProp
     public render() {
         const w = this.props.workflow;
         const creatorLabels = [];
-        const creatorKeys = new Map<string, string>([
-            [labels.creator, w.metadata.labels[labels.creator]],
-            [labels.creatorEmail, w.metadata.labels[labels.creatorEmail]],
-            [labels.creatorPreferredUsername, w.metadata.labels[labels.creatorPreferredUsername]]
-        ]);
-        creatorLabels.push(
-            Object.entries(creatorKeys).map(([key, value]) => (
-                <div
-                    title={`List workflows created by ${key}=${value}`}
-                    className='tag'
-                    key={`${w.metadata.uid}-${key}`}
-                    onClick={async e => {
-                        e.preventDefault();
-                        this.props.onChange(key, value);
-                    }}>
-                    <div className='key'>{key}</div>
-                    <div className='value'>{value}</div>
-                </div>
-            ))
-        );
-
+        if (w.metadata.labels) {
+            const creatorInfoMap = new Map<string, string>([
+                ['Name', w.metadata.labels[labels.creator]],
+                ['Email', w.metadata.labels[labels.creatorEmail]],
+                ['Preferred user name', w.metadata.labels[labels.creatorPreferredUsername]]
+            ]);
+            creatorLabels.push(
+                Object.entries(creatorInfoMap).map(([key, value]) => (
+                    <div
+                        title={`List workflows created by ${key}=${value}`}
+                        className='tag'
+                        key={`${w.metadata.uid}-${key}`}
+                        onClick={async e => {
+                            e.preventDefault();
+                            this.props.onChange(key, value);
+                        }}>
+                        <div className='key'>{key}</div>
+                        <div className='value'>{value}</div>
+                    </div>
+                ))
+            );
+        } else {
+            creatorLabels.push(<div key={`${w.metadata.uid}- `}> No creator information </div>);
+        }
         return <div className='wf-row-creator-labels'>{creatorLabels}</div>;
     }
 }
